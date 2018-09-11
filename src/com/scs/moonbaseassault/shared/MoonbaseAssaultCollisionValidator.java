ca0c37229b79e3df709c6b7e3b25bcd6be8968b0
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2018-09-11 11:08:30 +0100

    Don't collide if marked for removal

--- src/com/scs/moonbaseassault/shared/MoonbaseAssaultCollisionValidator.java
+++ src/com/scs/moonbaseassault/shared/MoonbaseAssaultCollisionValidator.java
@@ -10,32 +10,33 @@ public class MoonbaseAssaultCollisionValidator extends AbstractCollisionValidato
 
 	@Override
 	public boolean canCollide(PhysicalEntity pa, PhysicalEntity pb) {
-		// Explosion shards don't collide with each other
-		if (pa.type == Globals.EXPLOSION_SHARD && pb.type == Globals.EXPLOSION_SHARD) {
-			return false;
-		}
+		if (super.canCollide(pa, pb)) {		
+			// Explosion shards don't collide with each other
+			if (pa.type == Globals.EXPLOSION_SHARD && pb.type == Globals.EXPLOSION_SHARD) {
+				return false;
+			}
 
-		// Explosion shards don't collide with player
-		if ((pa.type == Globals.EXPLOSION_SHARD && pb.type == MoonbaseAssaultClientEntityCreator.SOLDIER_AVATAR) || (pb.type == Globals.EXPLOSION_SHARD && pa.type == MoonbaseAssaultClientEntityCreator.SOLDIER_AVATAR)) {
-			return false;
-		}
+			// Explosion shards don't collide with player
+			if ((pa.type == Globals.EXPLOSION_SHARD && pb.type == MoonbaseAssaultClientEntityCreator.SOLDIER_AVATAR) || (pb.type == Globals.EXPLOSION_SHARD && pa.type == MoonbaseAssaultClientEntityCreator.SOLDIER_AVATAR)) {
+				return false;
+			}
 
-		// Explosion shards don't collide with AI
-		if ((pa.type == Globals.EXPLOSION_SHARD && pb.type == MoonbaseAssaultClientEntityCreator.AI_SOLDIER) || (pb.type == Globals.EXPLOSION_SHARD && pa.type == MoonbaseAssaultClientEntityCreator.AI_SOLDIER)) {
-			return false;
-		}
+			// Explosion shards don't collide with AI
+			if ((pa.type == Globals.EXPLOSION_SHARD && pb.type == MoonbaseAssaultClientEntityCreator.AI_SOLDIER) || (pb.type == Globals.EXPLOSION_SHARD && pa.type == MoonbaseAssaultClientEntityCreator.AI_SOLDIER)) {
+				return false;
+			}
 
-		// Sliding doors shouldn't collide with floor/ceiling
-		if ((pa.type == MoonbaseAssaultClientEntityCreator.FLOOR_OR_CEILING && pb.type == MoonbaseAssaultClientEntityCreator.DOOR) || pa.type == MoonbaseAssaultClientEntityCreator.DOOR && pb.type == MoonbaseAssaultClientEntityCreator.FLOOR_OR_CEILING) {
-			return false;
-		}
-		// Sliding doors shouldn't collide with wall
-		if ((pa.type == MoonbaseAssaultClientEntityCreator.WALL && pb.type == MoonbaseAssaultClientEntityCreator.DOOR) || pa.type == MoonbaseAssaultClientEntityCreator.DOOR && pb.type == MoonbaseAssaultClientEntityCreator.WALL) {
-			return false;
-		}
-		
-		return super.canCollide(pa, pb);
+			// Sliding doors shouldn't collide with floor/ceiling
+			if ((pa.type == MoonbaseAssaultClientEntityCreator.FLOOR_OR_CEILING && pb.type == MoonbaseAssaultClientEntityCreator.DOOR) || pa.type == MoonbaseAssaultClientEntityCreator.DOOR && pb.type == MoonbaseAssaultClientEntityCreator.FLOOR_OR_CEILING) {
+				return false;
+			}
+			// Sliding doors shouldn't collide with wall
+			if ((pa.type == MoonbaseAssaultClientEntityCreator.WALL && pb.type == MoonbaseAssaultClientEntityCreator.DOOR) || pa.type == MoonbaseAssaultClientEntityCreator.DOOR && pb.type == MoonbaseAssaultClientEntityCreator.WALL) {
+				return false;
+			}
 
+		}
+		return true;
 	}
 
 }
